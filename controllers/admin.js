71b8aa0781a613815d41fac6d4be31de455e2fe6
Michael Manlulu <michael.manlulu@myresman.com>
2022-09-29 17:54:38 +0800

    217. deleting products

--- controllers/admin.js
+++ controllers/admin.js
@@ -84,7 +84,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId)
+  Product.findByIdAndDelete(prodId)
     .then((result) => {
       res.redirect('/admin/products');
     })
