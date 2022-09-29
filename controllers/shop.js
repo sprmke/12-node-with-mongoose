79dbfa3093809af85b6bcc53c5d13cf8a2a2d382
Michael Manlulu <michael.manlulu@myresman.com>
2022-09-29 20:18:57 +0800

    222. loading the cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -45,8 +45,10 @@ exports.getIndex = (req, res, next) => {
 
 exports.getCart = (req, res, next) => {
   req.user
-    .getCart()
-    .then((products) => {
+    .populate('cart.items.productId')
+    // .execPopulate()  Mongoose populate method returns as promise now
+    .then((user) => {
+      const products = user.cart.items;
       res.render('shop/cart', {
         path: '/cart',
         pageTitle: 'Your Cart',
