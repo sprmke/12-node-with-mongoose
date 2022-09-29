e6043ffd1709f43a54af922465c01f7e1253354e
Michael Manlulu <michael.manlulu@myresman.com>
2022-09-29 18:45:32 +0800

    220. important note about fetching relations (select & populate)

--- controllers/admin.js
+++ controllers/admin.js
@@ -73,7 +73,10 @@ exports.postEditProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
   Product.find()
+    // .select('title price description -_id')
+    // .populate('userId', 'name email -_id')
     .then((products) => {
+      console.log(products);
       res.render('admin/products', {
         prods: products,
         pageTitle: 'Admin Products',
